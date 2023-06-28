8889a0d167ec0257ffa2a637482ce1db7d4b55da
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-06-28 10:58:31 +0530

    Signals in solid basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
-
+import { createSignal } from 'solid-js';
+// const [count, setCount] = createSignal(1);
 function App() {
+
+  const [count, setCount] = createSignal(0);
+  setInterval(() => {
+    setCount(() => count() + 1);
+  }, 3000);
   return (
     <div class={styles.App}>
-      
+      <h1>Count - {count()}</h1>
     </div>
   );
 }
