f07990c8230d9ac2fbca0e79a26b0b9e0fd5aeeb
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-06-28 22:33:14 +0530

    Event handling in solid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { createSignal } from 'solid-js';
 
 function App() {
+
+  const [name, setName] = createSignal('Sai');
+
+  const nameUpdate = (e, data) => {
+    setName(e.currentTarget.value);
+  }
   return (
     <div class={styles.App}>
-      
+      <h2>Name - {name()}</h2>
+      <button onclick={() => setName('sai' + Math.random().toString())}>Change name</button>
+      {/* <input value={name()} onInput={(event) => setName(event.target.value)} placeholder={'Enter some name'}/> */}
+
+      <input type="text" value={name()} onInput={(e) => nameUpdate(e, 'kumar')} placeholder={'Enter some name'}/>
     </div>
   );
 }
