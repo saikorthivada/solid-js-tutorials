0798da893c8f16ee6454ec586534af21aca42069
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-06-29 19:27:38 +0530

    Looping statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { createSignal, For, Index } from 'solid-js';
 
 function App() {
+  const [names, setNames] = createSignal(["sai", "kumar", "korthivada"]);
   return (
     <div class={styles.App}>
-      
+      {/* {
+        names().map((name) => {
+          return <p>{name}</p>
+        })
+      } */}
+      {/* <For each={names()}>
+        {
+          (name, index) => {
+            return <p>{name} - {index}</p>
+          }
+        }
+      </For> */}
+
+      <Index each={names()}>
+        {
+          (name, index) => {
+            return <p>{name} - {index}</p>
+          }
+        }
+      </Index>
     </div>
   );
 }
