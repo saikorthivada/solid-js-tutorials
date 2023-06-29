94512c688bc4ed114f2d544e6656eab6ba0d920c
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-06-29 13:52:40 +0530

    Implemented show component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { createSignal, Show } from 'solid-js';
+import Marked from './Marked';
 
 function App() {
+  const [show, setShow] = createSignal(false);
   return (
     <div class={styles.App}>
-      
+      {/* <p>{show() ? 'Marked' : 'Not Marked'}</p> */}
+      <Show when={show()}>
+        <Marked />
+      </Show>
+
+      {/* {show() && <Marked />} */}
+      <button onclick={() => setShow(true)}>Mark as show</button>
     </div>
   );
 }
