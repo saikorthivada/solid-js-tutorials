ee464d8c29647f720819cd3420c98d4092462fac
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-07-05 21:05:56 +0530

    effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { createSignal } from 'solid-js';
+import { effect } from 'solid-js/web';
 
 function App() {
+  const [age, setAge] = createSignal(10);
+  const [count, setCount] = createSignal(0);
+
+  effect(() => {
+    console.log('My age has been updated', age());
+  });
+
+  effect(() => {
+    console.log('My count has been updated', count());
+  })
+
+  effect(() => {
+    console.log('My age / count has been updated', count(), age());
+  })
+
   return (
     <div class={styles.App}>
-      
+      <p>Age - {age()}</p>
+      <button onClick={() => setAge(age() + 1)}>Update age</button>
+      <button onClick={() => setCount(count() + 1)}>Update count</button>
     </div>
   );
 }
