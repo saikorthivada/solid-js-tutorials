c9047d5c16398a55c542c11c8015b33ab9aa2a09
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-07-05 11:44:43 +0530

    Derived signals

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { createSignal } from 'solid-js';
 
 function App() {
+  const [age, setAge] = createSignal(10);
+  const [multiplier, setMultiplier] = createSignal(2);
+
+  const doubleAge = () => age() * multiplier();
   return (
     <div class={styles.App}>
-      
+      <p>Age - {age()}</p>
+      <p>doubleAge - {doubleAge()}</p>
+      <button onClick={() => setAge(age() + 1)}>Increment age value</button>
+      <button onClick={() => setMultiplier(multiplier() + 1)}>Increment Multiplier value</button>
     </div>
   );
 }
