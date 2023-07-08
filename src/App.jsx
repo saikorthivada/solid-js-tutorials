47aa59b10e7a71891582fc25d047a6527991baab
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-07-08 07:45:53 +0530

    Untrack Signals

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { createEffect, createSignal, untrack } from 'solid-js';
 
 function App() {
+  const [age, setAge] = createSignal(10);
+  const [count, setCount] = createSignal(0);
+  // const [totalValue, setTotalVale] = createSignal(0);
+
+  const totalValue = () => age() + untrack(count);
+
+  // createEffect(() => {
+  //   console.log('inside effect');
+  //   setTotalVale(age() + untrack(count));
+  // });
+
   return (
     <div class={styles.App}>
-      
+      <p>Age - {age()}</p>
+      <p>Count - {count()}</p>
+      <p>Total - {totalValue()}</p>
+      <button onClick={() => setAge(age() + 1)}>Update Age</button>
+      <button onClick={() => setCount(count() + 1)}>Update Count</button>
     </div>
   );
 }
