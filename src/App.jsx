7fb93acff1fc8d55e0fc20ca26a95910cd1b3c5e
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-07-13 21:42:06 +0530

    Memos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { createMemo, createSignal, Index } from 'solid-js';
+
+function incrementByValue(input) {
+  return input + 1;
+}
 
 function App() {
+  const [count, setCount] = createSignal(0);
+  const [arr, _] = createSignal([1, 2, 3, 4, 5]);
+
+
+
+  const memoValue = createMemo(() => {
+    console.log('with memo');
+    return incrementByValue(count());
+  });
   return (
     <div class={styles.App}>
-      
+      <Index each={arr()}>
+        {
+          (value) => {
+            return <p>{value()}. {memoValue()}</p>
+          }
+        }
+      </Index>
+      <button onclick={() => setCount(count() + 1)}>Update count</button>
     </div>
   );
 }
