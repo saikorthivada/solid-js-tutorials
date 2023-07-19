59b932ae80f9cc8561978e0e2a18cd65e1c8bc86
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-07-19 18:26:24 +0530

    Store values updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
-
+import { createSignal } from 'solid-js';
+import {createStore} from 'solid-js/store';
 function App() {
+  // const [userDetails, setUserDetails] = createSignal({
+  //   name: 'sai',
+  //   age: 10
+  // });
+
+  const [userDetails, setUserDetails] = createStore({
+    name: 'sai',
+    age: 10
+  });
+
+  const updateName = () => {
+    // setUserDetails({...userDetails(), name: Math.random().toString()});
+    setUserDetails("name", Math.random().toString());
+  }
+
+  const updateAge = () => {
+    // setUserDetails({...userDetails(), age: userDetails().age + 1});
+    setUserDetails("age", (currentAge) => currentAge + 1);
+  }
   return (
     <div class={styles.App}>
-      
+      <p>Name - {userDetails.name}</p>
+      <p>Age - {userDetails.age}</p>
+      <button onClick={() => updateName()}>Update Name</button>
+      <button onClick={() => updateAge()}>Update Age</button>
     </div>
   );
 }
