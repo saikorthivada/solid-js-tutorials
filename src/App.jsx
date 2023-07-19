f27e14241ef840720eccb9b7c77e63a2c52ee901
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-07-19 18:41:20 +0530

    advance create store

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,36 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
 import { createSignal, For, Index } from 'solid-js';
+import { createStore } from 'solid-js/store';
 
 function App() {
-  const [users, setUsers] = createSignal([]);
+  const [users, setUsers] = createStore([]);
 
   const addAUser = () => {
-    const [isDeleted, setIsDeleted] = createSignal(false);
-    setUsers([...users(), {
-      id: users().length + 1,
-      name: `Sai ${users().length + 1}`,
-      isDeleted,
-      setIsDeleted
+    setUsers([...users, {
+      id: users.length + 1,
+      name: `Sai ${users.length + 1}`,
+      isDeleted: false
     }]);
   }
 
   const deleteUserById = (id) => {
-    const user = users().find(obj => obj.id === id);
-    if(user) {
-      user.setIsDeleted(true);
-    }
-    // setUsers(
-    //   users().map((obj) => {
-    //     if(obj.id === id) {
-    //       return {
-    //         ...obj,
-    //         isDeleted: true
-    //       }
-    //     } else {
-    //       return obj
-    //     }
-    //   })
-    // )
+    setUsers(
+      (user) => user.id === id,
+      "isDeleted",
+      true
+    );
   }
   return (
     <div class={styles.App}>
       <button onClick={() => addAUser()}>Add a random user</button>
-      <For each={users()}>
+      <For each={users}>
         {
           (user) => {
             console.log('Inside loop');
             return (
               <div>
-                <p style={{"text-decoration": user.isDeleted() ? "line-through" : "none"}}>{user.name}</p>
+                <p style={{ "text-decoration": user.isDeleted ? "line-through" : "none" }}>{user.name}</p>
                 <button onclick={() => deleteUserById(user.id)}>Delete User - {user.id}</button>
               </div>
             )
