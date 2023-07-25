b1b36edc6ae5d3cf64e614286a8d21e1b4bd67e6
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-07-25 21:54:08 +0530

    Refs

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 
 function App() {
+  let sampleRef;
+
+  const accessValue = () => {
+    // console.log(sampleRef.value);
+    sampleRef.focus();
+  }
   return (
     <div class={styles.App}>
-      
+      <input ref={sampleRef} placeholder="test refs"/>
+
+      <button onclick={() => accessValue()}>Access a value</button>
     </div>
   );
 }
