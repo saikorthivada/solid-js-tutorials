24a874d426c1b490c796bb2f290fde060b0b7316
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-08-02 19:20:12 +0530

    Create resource part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { createResource, Index } from 'solid-js';
 
+const fetchData = async () => {
+  return (await fetch('https://jsonplaceholder.typicode.co/todo')).json();
+}
 function App() {
+
+  const [users] = createResource(fetchData);
   return (
     <div class={styles.App}>
-      
+      {
+        users.loading ? <p>Loading ... </p> :
+        users.error ? <p>Error...</p> : 
+          <Index each={users()}>
+            {
+              (user) => {
+                return <p>{user().title}</p>
+              }
+            }
+          </Index>
+      }
+
     </div>
   );
 }
